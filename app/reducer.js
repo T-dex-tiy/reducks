9565553d4968681d2c77b4b4680208120e83687c
T-Dex <dex.m.mills@gmail.com>
2017-04-12 12:19:47 -0600

    feat (main) Add headers

--- app/reducer.js
+++ app/reducer.js
@@ -1,10 +1,18 @@
 module.exports = function reducer(state, action){
-  return[
-    ...state,
-    {
-    id: state[state.length - 1] ? state[state.length - 1].id + 1 : 1,
-    color: action.color,
-    size: action.size,
-    motto: action.motto
+  switch (action.type){
+    case 'SHIRT@ADD':
+      return[
+      ...state,
+      {
+      id: state[state.length - 1] ? state[state.length - 1].id + 1 : 1,
+      color: action.color,
+      size: action.size,
+      motto: action.motto
   }]
+  case 'SHIRT@REMOVE':
+      return state.filter( shirt => shirt.id !== action.id)
+  default:
+    return state
+    
+  }
 }
--- tests/reducer.test.js
+++ tests/reducer.test.js
@@ -104,3 +104,44 @@ test('SHIRT@ADD: add red sm to a state with two shirts', (t) => {
   t.deepEqual(reducer(starting, action), expect)
   t.end()
 })
+
+test('SHIRT@REMOVE: remove id 8 to a state with two shirts', (t) => {
+  const starting = [{
+    id: 2,
+    color: 'red',
+    size: 'sm',
+    motto: 'Keep calm and code on!'
+    }, {
+    id: 7,
+    color: 'white',
+    size: 'lg',
+    motto: 'Keep calm and code on!'
+    },
+    {
+    id: 8,
+    color: 'red',
+    size: 'sm',
+    motto: 'Real programmers count from 0.'
+    }]
+  const action = {
+    type: 'SHIRT@REMOVE',
+    id: 8,
+    color: 'red',
+    size: 'sm',
+    motto: 'Real programmers count from 0.'
+  }
+  const expect = [{
+    id: 2,
+    color: 'red',
+    size: 'sm',
+    motto: 'Keep calm and code on!'
+    }, {
+    id: 7,
+    color: 'white',
+    size: 'lg',
+    motto: 'Keep calm and code on!'
+    }]
+
+  t.deepEqual(reducer(starting, action), expect)
+  t.end()
+})
